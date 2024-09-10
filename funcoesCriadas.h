9ad588daf60bcbcc5df79397e8e8f18fcf8577dc
MiguelRF <fonsecamiguel0069@usp.br>
2024-09-10 11:32:52 -0300

    REGISTROS

--- funcoesCriadas.h
+++ funcoesCriadas.h
@@ -45,20 +45,6 @@ typedef struct
 
 //--------FUNCOES-DO-PROGRAMA------------
 
-//FUNCOES PADRAO, REDIRECIONAMENTO
-//void direcionaComando(int codigo);                              //FUNCAO DE SELECAO DE FUNCAO DE ACORDO COM O COMANDO
-
-
-//FUNCOES DE GRAVACAO/EDICAO DE ARQUIVOS (comandos 1 e 4) 
-//void registrar_especie();                                       //FUNCAO QUE LE DO TECLADO E ESCREVE NO ARQUIVO
-//void registra_informacao();                                     //FUNCAO QUE EDITA A INFORMACAO EM REGISTROS ESCOLHIDOS
-
-
-//FUNCOES DE RELATORIO (comandos 2 e 3)
-//void relatorio_especies();                      //FUNCOES QUE PRINTAM O RELATORIO DE ESPECIES DE TODO UM ARQUIVO
-//void relatorio_RRN();                           //FUNCAO QUE EXIBE O RELATORIO DE UMA ESPECIE ESPECIFICA DE ACORDO COM O RRN
-//void exibe_registro(registro_especie REGISTRO); //FUNCAO QUE PRINTA UM REGISTRO NA TELA
-
 
 
 #endif // __FUNCOES__
