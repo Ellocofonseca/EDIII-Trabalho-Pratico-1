bee760607597169432733ac70246baef99580eeb
MiguelRF <fonsecamiguel0069@usp.br>
2024-09-10 11:34:14 -0300

    .h

--- main.c
+++ main.c
@@ -1,18 +1,13 @@
 /*******************************************************************
- * @brief   Exercicio Introdutorio - Estruturas de dados 3
+ * @brief   Trabalho Pratico 1 - Estruturas de dados 3
  * @authors Miguel Rodrigues Fonseca, Bruno Calabria Cortez Navas
  * @date    02/07/2024
  * @note    Trabalho de ED3, quarto periodo, USP Sao Carlos
  *******************************************************************/
 #include "./funcoesCriadas.h"
-#include "./funcoesFornecidas.h"
 
 int main(){
 
-    int comando;                //variavel de comandod
-
-    scanf("%d",&comando);       //leitura do comando que sera executado
-    direcionaComando(comando);  //direciona o programa para cada funcao de acordo com o nro de comando
 
     return 0;
 }
